5e8d7cd9f4934c9d1e64c71b4228b18268fb255d
jeremiahMiclat <mic19017@byui.edu>
2023-05-17 23:31:18 +0800

    used querySelector instead of getElement

--- src/js/cart.js
+++ src/js/cart.js
@@ -5,12 +5,12 @@ function renderCartContents() {
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
   if (cartItems) {
-    var cartTotalContainer = document.getElementById("cart-total");
+    var cartTotalContainer = document.querySelector(".cart-footer");
     cartTotalContainer.classList.remove("hide");
     var cartTotalPrice = cartItems.reduce(function (accumulator, currentItem) {
       return accumulator + currentItem.FinalPrice;
     }, 0);
-    var cartTotal = document.getElementsByClassName("cart-total")[0];
+    var cartTotal = document.querySelector(".cart-total");
     cartTotal.textContent += cartTotalPrice;
   }
 }
