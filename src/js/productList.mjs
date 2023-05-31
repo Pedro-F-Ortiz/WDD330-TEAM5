1523ae16a466272484b135043d761efbbb2a0ee0
jeremiahMiclat <mic19017@byui.edu>
2023-05-31 20:41:28 +0800

    used if statement for SuggestedRetailPrice

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -2,6 +2,15 @@ import { getData } from "./productData.mjs";
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
+
+  let priceHTML = "";
+
+  if (product.SuggestedRetailPrice > product.FinalPrice) {
+    priceHTML = `<strike>$${product.SuggestedRetailPrice}</strike> $${product.FinalPrice}`;
+  } else {
+    priceHTML = `$${product.FinalPrice}`;
+  }
+
   return `<li class="product-card">
     <a href="product_pages/index.html?product=${product.Id}">
     <img
@@ -10,7 +19,7 @@ function productCardTemplate(product) {
     />
     <h3 class="card__brand">${product.Brand.Name}</h3>
     <h2 class="card__name">${product.NameWithoutBrand}</h2>
-    <p class="product-card__price"><strike>$${product.SuggestedRetailPrice}</strike> $${product.FinalPrice}</p></a>
+    <p class="product-card__price">${priceHTML}</p></a>
   </li>`;
 }
 
